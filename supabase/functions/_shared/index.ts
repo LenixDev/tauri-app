fe109534969f5584fd915031fe5234061410ba2a
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-17 17:49:54 +0100

    refactor(connection): streamline Edge class and improve Supabase client initialization

--- supabase/functions/_shared/index.ts
+++ supabase/functions/_shared/index.ts
@@ -3,7 +3,7 @@
 // This enables autocomplete, go to definition, etc.
 
 // Setup type definitions for built-in Supabase Runtime APIs
-import { createClient, SupabaseClient, User } from 'jsr:@supabase/supabase-js@2'
+import { createClient, SupabaseClient } from 'jsr:@supabase/supabase-js@2'
 import type { Events, Permission, Role, RealtimeRegisteration } from './types.ts';
 
 const corsHeaders = {
@@ -11,51 +11,51 @@ const corsHeaders = {
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
 }
 
-class Server {
-  protected accessor url: string
-  protected accessor key: string
-  protected constructor() {
-    this.url = this.initUrl()
-    this.key = this.initKey()
-  }
-
-  private initUrl() {
-    const url = Deno.env.get('SUPABASE_URL')
-    if (!url) throw new Error('SUPABASE_URL is not defined')
-    return url
-  }
-  private initKey() {
-    const key = Deno.env.get('SUPABASE_ANON_KEY')
-    if (!key) throw new Error('SUPABASE_ANON_KEY is not defined')
-    return key
-  }
-}
-
-export class UserConnection extends Server {
+class Edge {
   private readonly req: Request
-  private readonly permission: Permission
-  private readonly supabaseClient: SupabaseClient
-  private readonly admin: SupabaseClient
+  protected static readonly url: string = this.getENV("SUPABASE_URL")
+  protected static readonly key: string = this.getENV("SUPABASE_ANON_KEY")
+  protected readonly supabaseClient: SupabaseClient
 
-  constructor(req: Request, permission: Permission) {
-    super()
+  protected constructor(req: Request) {
     this.req = req
-    this.permission = permission
-    this.supabaseClient = this.createSupabaseClient()
-    this.admin = createClient(this.url, this.key)
+    this.supabaseClient = this.createSupabaseClient(Edge.url, Edge.key)
   }
 
-  private createSupabaseClient() {
+  private static getENV(key: "SUPABASE_ANON_KEY" | "SUPABASE_URL") {
+    const value = Deno.env.get(key)
+    if (!value) throw new Error(`${key} is not defined`)
+    return value
+  }
+
+  private createSupabaseClient(url: string, key: string) {
     const Authorization = this.req.headers.get('Authorization')
     if (!Authorization) throw new Error('Authorization header is not defined')
 
-    return createClient(this.url, this.key, {
+    return createClient(url, key, {
       global: { headers: { Authorization } }
     })
   }
 
-  private async isSafeConnection(): Promise<[true] | [false, Response]> {
+  protected checkMethod(): [true] | [false, Response] {
     if (this.req.method === 'OPTIONS') return [false, new Response("Wrong Method", { status: 405, headers: corsHeaders })]
+    return [true]
+  }
+}
+
+export class UserConnection extends Edge {
+  private readonly permission: Permission
+  private readonly admin: SupabaseClient
+
+  constructor(req: Request, permission: Permission) {
+    super(req)
+    this.permission = permission
+    this.admin = createClient(Edge.url, Edge.key)
+  }
+
+  private async isSafeConnection(): Promise<[true] | [false, Response]> {
+    const [success, response] = this.checkMethod()
+    if (!success) return [false, response]
 
     const { data: { user } } = await this.supabaseClient.auth.getUser()
     if (!user) return [false, new Response("Bad request", { status: 400, headers: corsHeaders })]
@@ -78,7 +78,7 @@ export class UserConnection extends Server {
     return [true]
   }
 
-  /** 
+  /**
    * Register the admin to the database's changes
    * @return
   */
