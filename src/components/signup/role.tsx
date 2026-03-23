3043f83ecd246bb80bd5dbb6c0f481da262420c2
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-03-23 22:28:48 +0100

    feat(components/signup/role.tsx): Add support for dir in RoleSelector component.

--- src/components/signup/role.tsx
+++ src/components/signup/role.tsx
@@ -12,32 +12,37 @@ import {
 } from "../ui/select"
 import type { OnChange } from "@/types"
 import { Star } from "../required"
+import { useDir } from "@/hooks/use-dir.ts";
 
-export const RoleSelector = ({ onChange }: { readonly onChange: OnChange }) => (
-  <Field>
-    <FieldLabel>
-      {t("role")}
-      <Star />
-    </FieldLabel>
-    <Select
-      defaultValue="student"
-      onValueChange={(value) => {
-        if (isRole(value)) onChange("role", value)
-      }}
-    >
-      <SelectTrigger className="w-full">
-        <SelectValue placeholder={t("signup.role")} />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>{t("roles")}</SelectLabel>
-          {Object.keys(ROLE_PERMISSIONS).map((role) => (
-            <SelectItem key={role} value={role}>
-              {t(`roles_alias.${role}`)}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  </Field>
-)
+export const RoleSelector = ({ onChange }: { readonly onChange: OnChange }) => {
+  const dir = useDir()
+  return (
+    <Field>
+      <FieldLabel>
+        {t("role")}
+        <Star />
+      </FieldLabel>
+      <Select
+        dir={dir}
+        defaultValue="student"
+        onValueChange={(value) => {
+          if (isRole(value)) onChange("role", value);
+        } }
+      >
+        <SelectTrigger dir={dir} className="w-full">
+          <SelectValue placeholder={t("signup.role")} />
+        </SelectTrigger>
+        <SelectContent dir={dir}>
+          <SelectGroup className="w-full">
+            <SelectLabel>{t("roles")}</SelectLabel>
+            {Object.keys(ROLE_PERMISSIONS).map((role) => (
+              <SelectItem key={role} value={role}>
+                {t(`roles_alias.${role}`)}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </Field>
+  );
+}
